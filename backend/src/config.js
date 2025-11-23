1d4dd5fb3204c60ccde4e58b65016f5f21ca3207
arifmac786 <arifmac78692@gmail.com>
2025-11-23 20:28:19 +0530

    - Added Mongoose schema for User collection - Includes email, fullName, password, and profilePic fields - Enabled timestamps for createdAt and updatedAt

--- backend/src/config.js
+++ backend/src/config.js
@@ -2,4 +2,6 @@ import dotenv from "dotenv";
 dotenv.config();
 export const mongoUri = process.env.MONGO_URI;
 export const PORT = process.env.PORT;
-export const corsOrigin = "http://localhost:5173";
+export const corsOrigin = process.env.CORS_ORIGIN;
+export const jwtSecretKey = process.env.JWT_SECRET_KEY;
+export const jwtExpiresIn = process.env.JWT_EXPIRES_IN;
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import { jwtExpiresIn, jwtSecretKey } from "../config.js";
 
 const userSchema = new mongoose.Schema(
   {
@@ -23,3 +26,19 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.comparePassword = async function (plainPassword) {
+  return await bcrypt.compare(plainPassword, this.password);
+};
+
+userSchema.methods.genAuthToken = function () {
+  return jwt.sign({ _id: this._id }, jwtSecretKey, { expiresIn: jwtExpiresIn });
+};
+
+const User = mongoose.model("User", userSchema);
+export default User;
